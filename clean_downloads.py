52d04bc404d522fe023873cde5706b428032e429
egilltor17 <egilltor17@ru.is>
2018-12-11 15:31:37 +0000

    Refactored regexfolders.py and addes sample and url filters to clean_downloads.py

--- clean_downloads.py
+++ clean_downloads.py
@@ -1,19 +1,33 @@
 from pathlib import Path
+from re import search, sub
+from regexfolders import regexes
 def main():
     # Will be user inputed: *********
     origin = "./Test Data/downloads"
     dest = "./Test Data/videos"
     # *******************************
 
-    pathList = getFileDirList(origin)
-    fileList = [i.name for i in pathList]
+    fileDirList = getFileDirList(origin)
+    filePartsList = [i.parts for i in fileDirList]
+    fileListNoSamples = sapleFilter(filePartsList)
+    fileList = urlFilterName(fileListNoSamples)
 
     return fileList
 
 
 def getFileDirList(origin):
-    fileExtentionWhitelist = ['.avi', '.flv', '.mp4', '.mkv', '.wmv', '.mov']
+    fileExtentionWhitelist = ['.avi', '.flv', '.mkv', '.mp4', '.mov', '.wmv']
     return [i.relative_to(origin) for i in Path(origin).glob('**/*') if i.suffix in fileExtentionWhitelist]
 
 
-print(main())
+def urlFilterName(fileDirList):
+    return [i if not search(regexes['url_detector'],i[-1]) else [i[:-1] + tuple(sub(regexes['url_detector'],"",i[-1]))] for i in fileDirList]
+    
+
+
+def sapleFilter(fileDirList):
+    return [i for i in fileDirList if len([j for j in i if search("[Ss]ample", j)]) == 0]
+
+
+for i in main():
+    print(i)
--- regexfolders.py
+++ regexfolders.py
@@ -1,8 +1,9 @@
 regexes = {
     'usual_Series_Format' : '[sS]\d{1,2}[eE ]+\d',
-    'series/Episode_Split_On_x' : '\d{1,2}x\d',
-    'series/Episode_Split_On_dot' : '\[\d{1,2}\.\d{1,2}\]',
+    'series_and_episode_split_on_x' : '\d{1,2}x\d',
+    'series_and_episode_split_on_dot' : '\[\d{1,2}\.\d{1,2}\]',
     'three_number_series/Episodes' : '([ \.\-_,]|^)\d{3}($|[ \.\-_,abc])',
-    'season_In_Name' : '[sS][eE][aA][sS][Oo][nN][ \.,_\-](\d|I|II|III|IV|V|VI|VII|VIII|IX|X){1,2}',
-    'StartsWithSeason' : '^[sS]\d{1,2}'
+    'season_in_name' : '[sS][eE][aA][sS][Oo][nN][ \.,_\-](\d|I|II|III|IV|V|VI|VII|VIII|IX|X){1,2}',
+    'starts_sith_season' : '^[sS]\d{1,2}',
+    'url_detector' : 'www\..+\.[A-Za-z]{2,3}\.?[A-Za-z]{1,3}'
 }
